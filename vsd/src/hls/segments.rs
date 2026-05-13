a9fb59597b84c155dfa73ce2004defc3a5ccbd69
clitic <clitic21@gmail.com>
2026-05-13 16:27:15 +0530

    better hls parsing 5

--- vsd/src/hls/segments.rs
+++ vsd/src/hls/segments.rs
@@ -7,6 +7,44 @@ fn parse_byte_range(br: &m3u8_rs::ByteRange, next_byte: &mut u64) -> playlist::R
     playlist::Range(start, end)
 }
 
+fn parse_key(key: m3u8_rs::Key) -> playlist::Key {
+    let mut method = match &key.method {
+        m3u8_rs::KeyMethod::AES128 => playlist::KeyMethod::Aes128,
+        m3u8_rs::KeyMethod::None => playlist::KeyMethod::None,
+        m3u8_rs::KeyMethod::SampleAES => playlist::KeyMethod::SampleAes,
+        m3u8_rs::KeyMethod::Other(x) if x == "SAMPLE-AES-CENC" || x == "SAMPLE-AES-CTR" => {
+            playlist::KeyMethod::Cenc
+        }
+        m3u8_rs::KeyMethod::Other(x) => playlist::KeyMethod::Other(x.to_owned()),
+    };
+
+    if method != playlist::KeyMethod::Cenc
+        && let Some(keyformat) = key.keyformat.as_deref()
+        && matches!(
+            keyformat,
+            "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
+                | "urn:uuid:9a04f079-9840-4286-ab92-e65be0885f95"
+                | "com.apple.streamingkeydelivery"
+        )
+    {
+        method = playlist::KeyMethod::Cenc;
+    }
+
+    if method != playlist::KeyMethod::Cenc
+        && let Some(uri) = key.uri.as_ref()
+        && (uri.starts_with("data:text/plain;base64,") || uri.starts_with("skd://"))
+    {
+        method = playlist::KeyMethod::Cenc;
+    }
+
+    playlist::Key {
+        default_kid: None,
+        iv: key.iv,
+        method,
+        uri: key.uri,
+    }
+}
+
 pub fn push_segments(stream: &mut playlist::MediaPlaylist, m3u8: m3u8_rs::MediaPlaylist) {
     stream.i_frame = m3u8.i_frames_only;
     stream.live = !m3u8.end_list;
@@ -24,47 +62,9 @@ pub fn push_segments(stream: &mut playlist::MediaPlaylist, m3u8: m3u8_rs::MediaP
             .byte_range
             .map(|br| parse_byte_range(&br, &mut next_byte));
 
-        let key = segment.key.map(|key| {
-            let mut method = match &key.method {
-                m3u8_rs::KeyMethod::AES128 => playlist::KeyMethod::Aes128,
-                m3u8_rs::KeyMethod::None => playlist::KeyMethod::None,
-                m3u8_rs::KeyMethod::SampleAES => playlist::KeyMethod::SampleAes,
-                m3u8_rs::KeyMethod::Other(x) if x == "SAMPLE-AES-CENC" || x == "SAMPLE-AES-CTR" => {
-                    playlist::KeyMethod::Cenc
-                }
-                m3u8_rs::KeyMethod::Other(x) => playlist::KeyMethod::Other(x.to_owned()),
-            };
-
-            if method == playlist::KeyMethod::None
-                && let Some(keyformat) = key.keyformat.as_deref()
-                && matches!(
-                    keyformat,
-                    "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
-                        | "urn:uuid:9a04f079-9840-4286-ab92-e65be0885f95"
-                        | "com.apple.streamingkeydelivery"
-                )
-            {
-                method = playlist::KeyMethod::Cenc;
-            }
-
-            if method == playlist::KeyMethod::None
-                && let Some(uri) = key.uri.as_ref()
-                && (uri.starts_with("data:text/plain;base64,") || uri.starts_with("skd://"))
-            {
-                method = playlist::KeyMethod::Cenc;
-            }
-
-            playlist::Key {
-                default_kid: None,
-                iv: key.iv,
-                method,
-                uri: key.uri,
-            }
-        });
-
         stream.segments.push(playlist::Segment {
             duration: segment.duration,
-            key,
+            key: segment.key.map(parse_key),
             map,
             range,
             uri: segment.uri,
