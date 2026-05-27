fc22fa6cdc2436db95ff20e44fbf63e2b4d2b7bc
clitic <clitic21@gmail.com>
2026-05-28 01:33:30 +0530

    added features docs

--- vsd/src/lib.rs
+++ vsd/src/lib.rs
@@ -1,38 +1,38 @@
-#![doc = "A command-line utility and library for downloading HLS and DASH video streams."]
-#![doc = ""]
-#![doc = "`vsd` enables concurrent chunk/segment downloading, automatic decryption of AES-128"]
-#![doc = "and Sample-AES encrypted streams, PSSH DRM metadata extraction, and automated muxing"]
-#![doc = "using ffmpeg."]
-#![doc = ""]
-#![doc = "# Cargo Features"]
-#![doc = ""]
-#![doc = "The following Cargo features can be enabled or disabled:"]
-#![doc = ""]
-#![doc = "| Feature | Description |"]
-#![doc = "|-------|-------------|"]
-#![doc = "| `capture` (*default*) | Enables the `capture` sub-command. |"]
-#![doc = "| `license` (*default*) | Enables the `license` sub-command. |"]
-#![doc = "| `rustls-tls` (*default*) | Enables the `rustls` TLS backend for the [reqwest] crate. |"]
-#![doc = "| `native-tls` | Enables the `native-tls` TLS backend for the [reqwest] crate. |"]
-#![doc = "| `native-tls-vendored` | Enables the `native-tls-vendored` TLS backend for the [reqwest] crate. |"]
-#![doc = ""]
-#![doc = "# Examples"]
-#![doc = ""]
-#![doc = "Below are examples demonstrating how to use the library to download files and playlists."]
-#![doc = ""]
-#![doc = "## `examples/playlist_dl.rs`"]
-#![doc = ""]
-#![doc = "```rust,no_run"]
+//! A command-line utility and library for downloading HLS and DASH video streams.
+//!
+//! `vsd` enables concurrent chunk/segment downloading, automatic decryption of AES-128
+//! and Sample-AES encrypted streams, PSSH DRM metadata extraction, and automated muxing
+//! using ffmpeg.
+//!
+//! # Cargo Features
+//!
+//! The following Cargo features can be enabled or disabled:
+//!
+//! | Feature | Description |
+//! |-------|-------------|
+//! | `capture` (*default*) | Enables the `capture` sub-command. |
+//! | `license` (*default*) | Enables the `license` sub-command. |
+//! | `rustls-tls` (*default*) | Enables the `rustls` TLS backend for the [reqwest] crate. |
+//! | `native-tls` | Enables the `native-tls` TLS backend for the [reqwest] crate. |
+//! | `native-tls-vendored` | Enables the `native-tls-vendored` TLS backend for the [reqwest] crate. |
+//!
+//! # Examples
+//!
+//! Below are examples demonstrating how to use the library to download files and playlists.
+//!
+//! ## `examples/playlist_dl.rs`
+//!
+//! ```rust,no_run
 #![doc = include_str!("../examples/playlist_dl.rs")]
-#![doc = "```"]
-#![doc = ""]
-#![doc = "## `examples/file_dl.rs`"]
-#![doc = ""]
-#![doc = "```rust,no_run"]
+//! ```
+//!
+//! ## `examples/file_dl.rs`
+//!
+//! ```rust,no_run
 #![doc = include_str!("../examples/file_dl.rs")]
-#![doc = "```"]
-#![doc = ""]
-#![doc = "[reqwest]: https://docs.rs/reqwest/latest/reqwest/#optional-features"]
+//! ```
+//!
+//! [reqwest]: https://docs.rs/reqwest/latest/reqwest/#optional-features
 
 #[macro_use]
 mod error;
